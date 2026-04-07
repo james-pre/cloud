34f653a09878719e9268f4e2ec0ef980221ce020
James Prevett <jp@jamespre.dev>
2026-04-06 21:59:53 -0500

    Updated ./use Removed dead code in svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,1 @@
-import main from '@axium/server/svelte.config.js';
-
-/** @type {import('@sveltejs/kit').Config} */
-export default {
-	...main,
-	kit: {
-		...main.kit,
-		alias: {
-			...main.kit.alias,
-			$cmp: './src/components',
-			$src: './src',
-		},
-	},
-};
+export { default as default } from '@axium/server/svelte.config.js';
